8460a9e86a89063f5145ee8ce4f3f2fd19637c78
afauzansw <afsw.fauzan33142@gmail.com>
2021-11-02 17:33:58 +0700

    :zap: install windmill react ui

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/" page={DashboardPage} name="dashboard" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -0,0 +1,18 @@
+// import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const DashboardPage = () => {
+  return (
+    <>
+      <MetaTags
+        title="Dashboard"
+        // description="Dashboard description"
+        /* you should un-comment description and add a unique description, 155 characters or less
+      You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
+      />
+      {/*  */}
+    </>
+  )
+}
+
+export default DashboardPage
